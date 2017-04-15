c4390b7db0063a35f80d48a9485b298ca6474f2d
jyuill <john@fig4.com>
2017-04-14 19:01:25 -0700

    testing pros/cons of different charting libraries in flexdashboards. No perfect solution! :[ ggplot2 might still be best overall.

--- tester.R
+++ tester.R
@@ -11,3 +11,16 @@ ggplot(mt, aes(x=disp, y=mpg))+geom_point()+
   facet_grid(.~gear)
 
 library(highcharter)
+hchart(mt, "scatter", hcaes(x = disp, y = mpg, group = gear))
+
+library(rCharts)
+
+p1 <- rPlot(data=mt, disp ~ mpg | gear, color='gear', type='point')
+p1 
+
+library(ggvis)
+mt %>% ggvis(x=~ disp, y=~mpg) %>% layer_points()
+mt %>% ggvis(x=~ disp, y=~mpg, fill=~factor(gear)) %>% layer_points()
+## you can add interaction but requires shiny
+mt %>% ggvis(x=~ disp, y=~mpg, fill=~factor(gear)) %>% layer_points() %>%
+  add_tooltip(function(df) paste("disp: ",df$disp,", mpg: ",df$mpg, sep=""))
